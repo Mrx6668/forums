a688757c9178c45e1175ea1122ecef2bf5a1ddc1
Mr_X <w2953873192@gamil.com>
2024-01-17 16:59:07 +0800

    init

--- front-end/src/views/indexView.vue
+++ front-end/src/views/indexView.vue
@@ -10,16 +10,9 @@
 </script>
 
 <template>
-  <div class="exit">
-    <el-button @click="userLogout" type="danger">退出登录</el-button>
-  </div>
+
 </template>
 
 <style scoped>
-.exit{
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
+
 </style>
